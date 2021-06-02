2acd5ffb55abc91b78f109ea8a214fb522d55b5f
Janae Quinones <janaemassa@gmail.com>
2021-06-01 21:46:40 -0400

    commit tree 802d95f85ce7c897e7ba1851e4b2cec50bf316c1

--- src/components/MovieReviews.js
+++ src/components/MovieReviews.js
@@ -1,1 +1,19 @@
 // Code MovieReviews Here
+import React from 'react'
+
+function MovieReviews({reviews}) {
+    // console.log(reviews)
+    return (
+        <div className='review-list'>
+            {reviews.map((review, i) => (
+                <div className='review' key={i}>
+                    <h3><a href={review.link.url}>{review.headline}</a> - {review.byline}</h3>
+                    <p>{review.summary_short}</p>
+                    <br/>
+                </div>
+            ))}
+        </div>
+    )
+}
+
+export default MovieReviews
